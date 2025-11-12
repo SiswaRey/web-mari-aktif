ff26a23e928da14d171247bc0cea1685ac7194f3
rafisulistyonur <rafisulistyonur@gmail.com>
2025-11-11 15:42:48 -1200

    update website for "Pernambah sistem pertemanan"

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
 const express = require('express');
 const { route } = require('./api.js');
+const fs = require('fs');
 
 const app = express();
 const port = 3000;
 
+function prosesHalaman(file) {
+    let data = fs.readFileSync(__dirname + '/pages/' + file + '.html', { encoding: 'utf8' });
+    
+    if (data.toString().includes('<komponen navbar></komponen>')) {
+        data = data.replaceAll('<komponen navbar></komponen>', fs.readFileSync(__dirname+'/pages/komponen/navbar.komponen.html', { encoding: 'utf8' }));
+    }
+
+    console.log(data)
+    return data
+}
 
 app.use("/api", route)
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/pages/utama.html');
+    res.send(prosesHalaman('utama'))
 });
 
 app.get('/public/:file', (req, res) => {
@@ -25,6 +36,9 @@ app.get("/login", (req, res) => {
     res.sendFile(__dirname + "/pages/login page.html")
 })
 
+app.get("/pertemanan", (req, res) => {
+    res.send(prosesHalaman('pertemanan'))
+})
 
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
---
+++ public/topNavigasi.js
@@ -0,0 +1,20 @@
+// Add active state on click
+document.querySelectorAll('.nav-link').forEach(link => {
+    link.addEventListener('click', function (e) {
+        // e.preventDefault();
+        document.querySelectorAll('.nav-link').forEach(l => l.classList.remove('active'));
+        this.classList.add('active');
+    });
+});
+
+// Search functionality demo
+document.querySelector('.search-input').addEventListener('keypress', function (e) {
+    if (e.key === 'Enter') {
+        alert('Mencari: ' + this.value);
+    }
+});
+
+// Add question button
+document.querySelector('.add-btn').addEventListener('click', function () {
+    alert('Fitur tambah pertanyaan akan dibuka');
+});
